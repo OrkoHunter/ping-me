1fb8f6993769a5e7807c9476998de1fd4004c73f
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-22 04:40:42 +0530

    Initialized setup and license

---
+++ setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup
+
+if sys.argv[-1] == 'setup.py':
+    print('To install, run \'python setup.py install\'')
+    print()
+
+NAME = 'ping-me'
+VERSION = '0.1'
+
+if __name__ == "__main__":
+    setup(
+        name = NAME,
+        version = VERSION,
+        author = 'Himanshu Mishra',
+        author_email = 'himanshumishra@iitkgp.ac.in',
+        description = 'Cross platform personalized ping',
+        long_description = open('README.md').read(),
+        packages = ['ping_me'],
+        license = 'Apache License',
+        entry_point = {
+            'console_scripts': [
+            'ping-me = ping_me.ping:main'
+            ]
+        },
+        test_suite = 'nose.collector',
+        tests_require = ['nose>=0.10.1']
+    )
