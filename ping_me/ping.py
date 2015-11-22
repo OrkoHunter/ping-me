ba01ca0adccf0bc095000f72709c79a8c16cb407
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-22 12:27:59 +0530

    Provide option for smart ping-me

--- ping_me/ping.py
+++ ping_me/ping.py
@@ -19,25 +19,31 @@ def main():
     hour = 'none'
     minute = 'none'
 
-    for opt, arg in optlist:
-        if opt == '-d':
-            for i in arg.split('-'):
-                try:
-                    if len(str(int(i))) == 4:
-                        year = int(i)
-                    elif len(str(int(i))) == 2 or len(str(int(i))) == 1:
-                        day = int(i)
-                except ValueError:
-                    month = i
-        elif opt == '-t':
-            hour, minute = int(arg.split(':')[0]), int(arg.split(':')[1])
-        else:
-            sys.exit(1)
+    if optlist != []:
+    # proper time and date are given using -d and -t flags
+        for opt, arg in optlist:
+            if opt == '-d':
+                for i in arg.split('-'):
+                    try:
+                        if len(str(int(i))) == 4:
+                            year = int(i)
+                        elif len(str(int(i))) == 2 or len(str(int(i))) == 1:
+                            day = int(i)
+                    except ValueError:
+                        month = i
+            elif opt == '-t':
+                hour, minute = int(arg.split(':')[0]), int(arg.split(':')[1])
+            else:
+                sys.exit(1)
 
-    if month != 'none':
-        month=month_key[month[:3].title()]
-    ping_me.engine(message, year=year, month=month,
-                   day=day, hour=hour, minute=minute)
+        if month != 'none':
+            month=month_key[month[:3].title()]
+        ping_me.engine(message, year=year, month=month,
+                       day=day, hour=hour, minute=minute)
+    else:
+        # Nothing is even, just a plain string.
+        # e.g. ping-me to call mom tonight
+        print("pint-me ain't that smart now. Use the flags instead.")
 
 if __name__ == "__main__":
 
