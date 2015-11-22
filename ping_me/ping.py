3d4888ecd644a059d681e24a12ecd2c8c680a12a
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-22 12:39:12 +0530

    Code refactor

--- ping_me/ping.py
+++ ping_me/ping.py
@@ -13,36 +13,37 @@ month_key = dict((v, k) for k, v in
 def main():
     optlist, message = getopt.getopt(sys.argv[1:], 'd:t:')
 
-    day = 'none'
-    month = 'none'
-    year = 'none'
-    hour = 0
-    minute = 0
+    day = month = year = 'none'
+    hour = minute = 0
 
     if optlist != []:
-    # proper time and date are given using -d and -t flags
+    # Proper time and date are given using -d and -t flags
         for opt, arg in optlist:
+            # Processing date
             if opt == '-d':
-                for i in arg.split('-'):
+                for i in arg.split('-'):  # e.g. November-25-2015
                     try:
+                    # Day and year would be integer convertible
                         if len(str(int(i))) == 4:
                             year = int(i)
                         elif len(str(int(i))) == 2 or len(str(int(i))) == 1:
                             day = int(i)
                     except ValueError:
+                        # Month would be just a string
                         month = i
+            # Processing time
             elif opt == '-t':
                 hour, minute = int(arg.split(':')[0]), int(arg.split(':')[1])
             else:
                 sys.exit(1)
 
         if month != 'none':
-            month=month_key[month[:3].title()]
+            month = month_key[month[:3].title()]
         ping_me.engine(message, year=year, month=month,
                        day=day, hour=hour, minute=minute)
     else:
         # Nothing is even, just a plain string.
-        # e.g. ping-me to call mom tonight
+        # $ ping-me to call mom tonight
         print("pint-me ain't that smart now. Use the flags instead.")
 
 if __name__ == "__main__":
