fd106b543b75c34eee5af5d3b78e0d2c5f2cbe7a
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-28 15:45:13 +0530

    Store email and password of new user

--- ping_me/__init__.py
+++ ping_me/__init__.py
@@ -1,2 +1,4 @@
 from ping_me.ping import *
 from ping_me.engine import *
+from ping_me.authenticate import *
+
---
+++ ping_me/authenticate.py
@@ -0,0 +1,39 @@
+"""The authentication module of ping-me"""
+
+import getpass
+import hashlib
+import re
+import sys
+
+def newuser():
+    EMAIL_REGEX = re.compile(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")
+
+    sys.stdout.write("Email : ")
+    email = sys.stdin.readline()
+    while(not EMAIL_REGEX.match(email)):
+        sys.stderr.write("Wrong email address. Try again.\n")
+        sys.stdout.write("Email : ")
+        email = sys.stdin.readline()
+    email = email.rstrip()  # Remove newline character
+
+    password = hashlib.md5(getpass.getpass().rstrip()).hexdigest()
+    repass = hashlib.md5(getpass.getpass("Re-enter : ").rstrip()).hexdigest()
+    if password != repass:
+        sys.stderr.write("Password match failed. Try again.\n")
+        password = hashlib.md5(getpass.getpass().rstrip()).hexdigest()
+        repass = hashlib.md5(getpass.getpass("Re-enter : " +
+            "").rstrip()).hexdigest()
+
+    # To create a file (Better hack?)
+    f = open('/home/' + getpass.getuser() + '/.pingmeconfig', 'w+')
+    f.close()
+
+    config_file = open('/home/' + getpass.getuser() + '/.pingmeconfig', 'r+')
+    config_file.write('[email]\n\t' + email + '\n')
+    config_file.write('[password]\n\t' + password + '\n')
+
+
+
+def olduser():
+    pass
+
