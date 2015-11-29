16a99732a982461569aa1f9d4a71646e1574c8d9
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-11-29 17:41:13 +0530

    Add the getter module

---
+++ cron/GET.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import urllib2
+import ping_me
+import hashlib
+import sys
+import subprocess
+import tunnelcrypt
+
+country = ping_me.authenticate.extract_phone()[2]
+filename = country[:2] + country[-2:]
+del country
+target = "http://www.himanshumishra.in/pingme/cron/" + filename + '.txt'
+email = ping_me.authenticate.extract_email()
+password = ping_me.authenticate.extract_password()
+hashed_email = hashlib.md5(email).hexdigest()
+del email
+data = urllib2.urlopen(target)
+
+
+for line in data:
+    line = line.split()
+    if line[0] == hashed_email:
+        for i in range(1, len(line)):
+            line[i] = int(line[i])
+        message = tunnelcrypt.decrypt(line[1:], password)
+        if sys.platform == 'linux2':
+            subprocess.call(['notify-send', message])
+        elif sys.platform == 'darwin':
+            subprocess.call(['terminal-notifier', '-title', 'ping-me', 'Ping!'])
+        elif sys.platform in ['win32', 'win64']:
+            # Do things for windows
+            pass
+
+del target
+del data
+del hashed_email
+
