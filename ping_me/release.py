1320ce639e1be054be30265f66e747f19bdaefe5
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-12-08 20:59:12 +0530

    Added release information

---
+++ ping_me/release.py
@@ -0,0 +1,14 @@
+"""Release file for ping-me"""
+
+name = 'ping-me'
+major = '0'
+minor = '2'
+
+__version__ = major + '.' + minor
+
+__date__ = '2015-12-08'  ## YYYY-MM-DD
+
+__discription__ = 'Cross platform personalized ping'
+
+__author__ = 'Himanshu Mishra'
+__email__ = 'himanshu2014iit@gmail.com'
