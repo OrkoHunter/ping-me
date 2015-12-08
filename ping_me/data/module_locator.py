14dfe9dea2e5549401f2929fa143cdfd89664bff
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-12-08 18:44:07 +0530

    Make package wheel proof

--- ping_me/data/module_locator.py
+++ ping_me/data/module_locator.py
@@ -1,4 +1,10 @@
-"""Locate the data files in the eggs to open"""
+"""Locate the data files in the eggs to open."""
+
+# NOTE : This module is redundant now. It was used to locate the csv file
+# present inside the installed egg. But as the wheels do not respect relative
+# path for data files, using a file may be dangerous and hence the data is
+# kept as a dictionary inside `countrylist.py`. The following code is not
+# deleted from the package because of love.
 
 import os
 import sys
