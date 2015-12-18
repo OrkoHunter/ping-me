fc93b471164e0fa07d064477aecffc6829c6582c
Himanshu Mishra <himanshu2014iit@gmail.com>
2015-12-18 19:58:04 +0530

    Modify GET.py for cron jobs

--- ping_me/GET.py
+++ ping_me/GET.py
@@ -5,40 +5,31 @@ import ast
 import requests
 import sys
 import subprocess
-import time
 
 from ping_me.utils import cryptex
 import ping_me.authenticate
 
 def main():
-    while(True):
-        target = "http://ping-me.himanshumishra.in/ping/"
-        email = ping_me.authenticate.extract_email()
-        key = ping_me.authenticate.extract_password()
-        data_t = {
-            "email" : email,
-            "password" : key
-        }
-        try:
-            r = requests.post(target, data=data_t)
-            if ast.literal_eval(r.text)["success"] == "True":
-                message = cryptex.decryptor(key, ast.literal_eval(r.text)["message"])
-                # Recieved the ping 5 seconds earlier
-                time.sleep(5)
-                if sys.platform == 'linux2':
-                    subprocess.call(['notify-send', message])
-                elif sys.platform == 'darwin':
-                    # Need to install `terminal-notifier`
-                    # $ brew install terminal-notifier
-                    subprocess.call(['terminal-notifier', '-title',
-                                     'ping-me', message])
-                elif sys.platform in ['win32', 'win64']:
-                    # Do things for windows
-                    pass
-            else:
-                time.sleep(1)
-        except Exception as e:
-            print(e)
+    target = "http://ping-me.himanshumishra.in/ping/"
+    email = ping_me.authenticate.extract_email()
+    key = ping_me.authenticate.extract_password()
+    data_t = {
+        "email" : email,
+        "password" : key
+    }
+    r = requests.post(target, data=data_t)
+    if ast.literal_eval(r.text)["success"] == "True":
+        message = cryptex.decryptor(key, ast.literal_eval(r.text)["message"])
+        if sys.platform == 'linux2':
+            subprocess.call(['notify-send', message])
+        elif sys.platform == 'darwin':
+            # Need to install `terminal-notifier`
+            # $ brew install terminal-notifier
+            subprocess.call(['terminal-notifier', '-title',
+                             'ping-me', message])
+        elif sys.platform in ['win32', 'win64']:
+            # Do things for windows
+            pass
 
 if __name__ == '__main__':
     main()
